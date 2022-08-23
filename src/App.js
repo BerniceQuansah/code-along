62fe51e7ae46ba67c4410a81586b690170625f59
Bernice Quansah <bernicequansah396@gmail.com>
2022-08-23 14:08:45 +0000

    routing-1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-// import Header  from "./components/Header"
-// import Button from "./components/Button"
-// import {Image} from "./components/Image"
-// import Navbar from "./components/Navbar";
-// import Practice from "./components/Practice";
-import Spinner from "./components/Spinner";
-import ProductList from "./components/ProductList";
-import TaskManager from "./components/TaskManager";
-// import Form from "./components/Form";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
-function App(){
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import ProductList from "./components/ProductList";
+import TaskManager from "./components/TaskManager"
+
+function App() {
   return (
-    <div>
-      {/* <Navbar />
-      <Header />
-      <Button label="Add to Cart" />
-      <Button label="Login" />
-      <Button label="Register" />
-      <Button label="Read more..." />
-      <Image /> */}
-      {/* <Practice /> */}
-      {/* <TaskManager /> */}
-       {/* <Form /> */}
-       <ProductList />
-    </div>
+   <BrowserRouter>
+   <Navbar />
+   <Routes>
+   <Route path="/" element={<Home />} />
+    <Route path="/product" element={<ProductList />} />
+    <Route path="/task-manager" element={<TaskManager />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+   </Routes>
+   </BrowserRouter>
   );
 }
 
