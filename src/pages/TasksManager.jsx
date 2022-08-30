c024eeca0cd33e26c8348d741b79a8fc51b027e0
Bernice Quansah <bernicequansah396@gmail.com>
2022-08-30 14:44:51 +0000

    state-management-3

--- src/pages/TasksManager.jsx
+++ src/pages/TasksManager.jsx
@@ -1,57 +1,88 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
+import TaskItem from '../components/TaskItem';
+import { v4 as uuid } from "uuid";
+import { useTaskContext } from '../context/taskContext';
 
-import { v4 as uuid} from "uuid";
 
-import TaskItem from "../components/TaskItem";
-import { useTaskContext } from "../context/taskContext";
 
 function TaskManager(){
    const { tasks, setValue } = useTaskContext();
-   const [input, setInput] = useState("");
+   const [input, setInput] = useState('');
+
 
 const handleSubmit = e => {
     e.preventDefault();
     if (input === "") return;
 
-    const newTask ={
+    const newTask = {
         id: uuid(),
         text: input,
-        completed: false
+        completed: false,
     };
+
      
      setValue([newTask, ...tasks]);
-     setInput("");
-     localStorage.setItem("tasks", JSON.stringify(tasks));
-};
-
-     const handleDelete = id => {
+     setInput('');
+    //  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+     const handleDelete = (id) => {
         const newTasks = tasks.filter((task) => task.id !== id);
+        setValue(newTasks);
+     }
+
+
+     const handleCompleted = (id) => {
+          const newTasks = tasks.map((task) => {
+               if(task.id === id){
+                  return {
+                    ...task,
+                    completed: !task.completed,
+                  }
+               }
+               return task;
+          })
+          setValue(newTasks);
+     };
+
+     const handleEdit = (id) => {
+        const newTasks = tasks.filter((task) => {
+            if (task.id === id){
+                setInput(task.text);
+                return false;
+            }
+            return task;
+        })
         setValue(newTasks);
      };
 
+
      useEffect(() => {
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-      }, [tasks]);
+       localStorage.setItem("tasks", JSON.stringify(tasks));
+     }, [tasks]);
 
 
     return(
         <div className='bg-blue-500 h-screen flex justify-center items-center'>
               <div className='max-w-xl max-h-96 bg-white rounded-xl px-5 py-10'>
-              <h3 className='mb-4'>Welcome</h3>
            <form onSubmit={handleSubmit} className="space-x-5 flex w-[30rem] mb-10">
                 <input type="text" className='border-2 border-blue-400 p-2 rounded-md outline-none w-10/12' onChange={(e) => setInput(e.target.value)} value = {input}/>
                 <button type = "submit" className='bg-blue-600 text-white text-lg py-2 px-5 rounded-md' disabled = {input === ""} >Add</button>
             </form>
          <div className="space-y-3 overflow-y-auto h-56">
-            { tasks.map((task) => (
-               <TaskItem key={task.id} task={task} handleDelete={handleDelete} />
-               ))}
+            {
+                tasks.map((task) =>  (<TaskItem 
+                    key={task.id} 
+                    task={task} 
+                    handleDelete={handleDelete}
+                    handleCompleted={handleCompleted}
+                    handleEdit={handleEdit}
+                    />))
+             }
         </div>
+
              </div>
         </div>
-    );
+    )
 };
 
 export default TaskManager;
-
-
